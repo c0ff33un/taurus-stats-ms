7ba870c8e14c9661476c40c4400fb60d431140a7
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-12-19 00:43:19 -0500

    All working 2.0

--- src/provide_data/rank.py
+++ src/provide_data/rank.py
@@ -19,12 +19,12 @@ def global_ranker():
 def user_rank(userId):
     db = get_conn_db()
     cursor = db.cursor()
-    # try:
-    query = 'SELECT * FROM ( SELECT userId, avgTime, @curRank := @curRank + 1 AS rank FROM user, (SELECT @curRank := 0) r where avgTime != 0 ORDER BY  avgTime) rank where rank.userId = \'{}\' limit 100;'.format(userId)
-    cursor.execute(query)
-    df = DataFrame(cursor.fetchall())
-    df.columns = ["userId", "avgTime", "rank"]
-    j = df.to_json(orient='index')
-    return json.loads(j)
-    # except:
-    #     return False
+    try:
+        query = 'SELECT * FROM ( SELECT userId, avgTime, @curRank := @curRank + 1 AS rank FROM user, (SELECT @curRank := 0) r where avgTime != 0 ORDER BY  avgTime) rank where rank.userId = \'{}\' limit 100;'.format(userId)
+        cursor.execute(query)
+        df = DataFrame(cursor.fetchall())
+        df.columns = ["userId", "avgTime", "rank"]
+        j = df.to_json(orient='index')
+        return json.loads(j)
+    except:
+        return False
